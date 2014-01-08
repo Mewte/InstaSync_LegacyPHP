2e9632dd1712b08cffd4356e9d4dd17846717db2
Mewte <admin@instasynch.com>
2014-01-08 08:27:49 -0600

    add access-control-allow-origin to template.php

--- includes/template.php
+++ includes/template.php
@@ -13,6 +13,7 @@
     }
 ?>
 <?php 
+    header('Access-Control-Allow-Origin: http://instasynch.com:38000'); 
     $roomname = str_replace("/", "", $_GET["filename"]);
     $visits = 0;
     $about = "";
