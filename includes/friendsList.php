003e651f374084ed2b462a982d6ac3a2cd403c66
Mewte <admin@instasynch.com>
2014-01-19 23:32:43 -0600

    more

--- includes/friendsList.php
+++ includes/friendsList.php
@@ -4,34 +4,49 @@
                 <div class="friend-count">4 Online</div>
                 <img class="friends-list-expand-icon" src="/images/social/expand.png" height="22" width="22"/>
             </div>
-            <div class="friendsList-list">
-                <div class="category">
-                    (<span id="friends-list-received-count">0</span>) Received
-                    <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
-                </div>
-                <ul class="received">
-                    <li></lI>
-                </ul>
-                <div class="category">
-                    (<span id="friends-list-sent-count">0</span>) Sent
-                    <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
-                </div>
-                <ul class="sent">  
-                </ul>
-                <div class="category">
-                    (<span id="friends-list-online-count">0</span>) Online
-                    <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
-                </div>
-                <ul class="online">       
-                </ul>
-                <div class="category">
-                    (<span id="friends-list-offline-count">0</span>) Offline
-                    <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
-                </div>
-                <ul class="offline">
-                    
-                </ul>
-            </div>
+            <ul class="friendsList-list">
+                <li>
+                    <div class="category">
+                        (<span id="friends-list-received-count">0</span>) Received
+                        <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
+                    </div>
+                    <ul class="category-list">
+                        <li>
+                            <div class="username">Mewte</div>
+                        </li>
+                        <li>
+                            <div class="username">Senpai</div>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <div class="category">
+                        (<span id="friends-list-sent-count">0</span>) Sent
+                        <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
+                    </div>
+                    <ul class="category-list">
+                        
+                    </ul>
+                </li>
+                <li>
+                    <div class="category">
+                        (<span id="friends-list-online-count">0</span>) Online
+                        <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
+                    </div>
+                    <ul class="category-list">
+                        
+                    </ul>
+                </li>
+                <li>
+                    <div class="category">
+                        (<span id="friends-list-offline-count">0</span>) Offline
+                        <img class="category-image" src="/images/social/plus.png" height="16" width="16" />
+                    </div>
+                    <ul class="category-list">
+
+                    </ul>
+                </li>
+            </ul>
         </div>
         <script type="text/javascript" src="/js/friends.js">
         </script>
